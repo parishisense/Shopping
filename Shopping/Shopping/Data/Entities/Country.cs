2c20663ab8f04f4b3c5f29f0da8953893a343c8a
parishisense <64501675+parishisense@users.noreply.github.com>
2022-05-09 15:38:35 -0500

    Clase 4: Crear BD, y Country y Controlador

---
+++ Shopping/Shopping/Data/Entities/Country.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shopping.Data.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="El Campo {0} es obligatorio.")]
+        [MaxLength(50, ErrorMessage ="El campo {0} debe tener máximo {1} caractéres.")]
+        [Display(Name ="País")]
+        public string Name { get; set; }
+    }
+}

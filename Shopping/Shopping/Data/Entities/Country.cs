b5b01a29ad2cb8e170b5b9c1f819212957abb5fa
PARIS IVAN BRAVO MEJIA <parisbravo@gmail.com>
2024-11-12 17:44:21 -0600

    Countries Controller

---
+++ Shopping/Shopping/Data/Entities/Country.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Shopping.Data.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        //Datanotation
+        [Display(Name = "País")]
+        [MaxLength(50, ErrorMessage ="El campo {0} debe tener máximo {1} caracteres")]
+        [Required(ErrorMessage ="El Campo {0} es obligatorio")]
+        public string Name { get; set; }
+    }
+}

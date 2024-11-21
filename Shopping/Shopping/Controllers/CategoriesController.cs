4efe70747a8480995d02b11264bf909be86daab0
PARIS IVAN BRAVO MEJIA <parisbravo@gmail.com>
2024-11-21 13:10:17 -0600

    Create CategoriesController - CRUD Finished

--- Shopping/Shopping/Controllers/CategoriesController.cs
+++ Shopping/Shopping/Controllers/CategoriesController.cs
@@ -3,61 +3,47 @@ using Microsoft.EntityFrameworkCore;
 using Shopping.Data;
 using Shopping.Data.Entities;
 
+
 namespace Shopping.Controllers
 {
-    public class CountriesController : Controller
+    public class CategoriesController : Controller
     {
         private readonly DataContext _context;
 
-        public CountriesController(DataContext context)
+        public CategoriesController(DataContext context)
         {
-            _context = context;
+            this._context = context;
         }
 
-        // GET: Countries
+
         //Index
 
         [HttpGet]
         public async Task<IActionResult> Index()
         {
-            return _context.Countries != null ?
-                        View(await _context.Countries.ToListAsync()) :
-                        Problem("Entity set 'DataContext.Countries'  is null.");
+            return View(await _context.Categories.ToListAsync());
         }
 
-        // GET: Countries/Details/5
-        [HttpGet]
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Countries == null)
-            {
-                return NotFound();
-            }
 
-            Country? country = await _context.Countries
-                .FirstOrDefaultAsync(m => m.Id == id);
-            return country == null ? NotFound() : View(country);
-        }
-
-        // GET: Countries/Create
+        // GET: Categories/Create
         [HttpGet]
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Countries/Create
+        // POST: Categories/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Country country)
+        public async Task<IActionResult> Create(Category category)
         {
             if (ModelState.IsValid)
             {
                 try
                 {
-                    _ = _context.Add(country);
+                    _ = _context.Add(category);
                     _ = await _context.SaveChangesAsync();
                     return RedirectToAction(nameof(Index));
                 }
@@ -65,7 +51,7 @@ namespace Shopping.Controllers
                 {
                     if (dbUpdateException.InnerException.Message.Contains("duplicate"))
                     {
-                        ModelState.AddModelError(string.Empty, "Ya existe un país con el mismo nombre.");
+                        ModelState.AddModelError(string.Empty, "Ya existe una categoria con el mismo nombre.");
                     }
                     else
                     {
@@ -77,31 +63,28 @@ namespace Shopping.Controllers
                     ModelState.AddModelError(string.Empty, exception.Message);
                 }
             }
-            return View(country);
+            return View(category);
         }
-
-
-
-        // GET: Countries/Edit/5
+        // GET: Categories/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null || _context.Countries == null)
+            if (id == null || _context.Categories == null)
             {
                 return NotFound();
             }
 
-            Country? country = await _context.Countries.FindAsync(id);
-            return country == null ? NotFound() : View(country);
+            Category? category = await _context.Categories.FindAsync(id);
+            return category == null ? NotFound() : View(category);
         }
 
-        // POST: Countries/Edit/5
+        // POST: Categories/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Country country)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Category category)
         {
-            if (id != country.Id)
+            if (id != category.Id)
             {
                 return NotFound();
             }
@@ -110,7 +93,7 @@ namespace Shopping.Controllers
             {
                 try
                 {
-                    _ = _context.Update(country);
+                    _ = _context.Update(category);
                     _ = await _context.SaveChangesAsync();
                     return RedirectToAction(nameof(Index));
                 }
@@ -118,7 +101,7 @@ namespace Shopping.Controllers
                 {
                     if (dbUpdateException.InnerException.Message.Contains("duplicate"))
                     {
-                        ModelState.AddModelError(string.Empty, "Ya existe un país con el mismo nombre.");
+                        ModelState.AddModelError(string.Empty, "Ya existe una categoria con el mismo nombre.");
                     }
                     else
                     {
@@ -130,41 +113,55 @@ namespace Shopping.Controllers
                     ModelState.AddModelError(string.Empty, exception.Message);
                 }
             }
-            return View(country);
+            return View(category);
         }
-
-        // GET: Countries/Delete/5
-        public async Task<IActionResult> Delete(int? id)
+        // GET: Categories/Details/5
+        [HttpGet]
+        public async Task<IActionResult> Details(int? id)
         {
-            if (id == null || _context.Countries == null)
+            if (id == null || _context.Categories == null)
             {
                 return NotFound();
             }
 
-            Country? country = await _context.Countries
+            Category? category = await _context.Categories
                 .FirstOrDefaultAsync(m => m.Id == id);
-            return country == null ? NotFound() : View(country);
+            return category == null ? NotFound() : View(category);
         }
 
-        // POST: Countries/Delete/5
+        // GET: Categories/Delete/5
+        //
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Categories == null)
+            {
+                return NotFound();
+            }
+
+            Category? category = await _context.Categories
+                .FirstOrDefaultAsync(m => m.Id == id);
+            return category == null ? NotFound() : View(category);
+        }
+
+        // POST: Categories/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            if (_context.Countries == null)
+            if (_context.Categories == null)
             {
                 return Problem("Entity set 'DataContext.Countries'  is null.");
             }
-            Country? country = await _context.Countries.FindAsync(id);
-            if (country != null)
+            Category? category = await _context.Categories.FindAsync(id);
+            if (category != null)
             {
-                _ = _context.Countries.Remove(country);
+                _ = _context.Categories.Remove(category);
             }
 
             _ = await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
-
-
     }
+
 }
+
---
+++ Shopping/Shopping/Data/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shopping.Data.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        //Datanotation
+        [Display(Name = "Categoria")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres")]
+        [Required(ErrorMessage = "El Campo {0} es obligatorio")]
+        public string Name { get; set; }
+    }
+}

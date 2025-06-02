d8f2c778b69c7b5059d4344731bb9ba40f0271c9
Paris <Paris@ASUSAMISTAD>
2025-06-02 17:28:32 -0600

    MyOrders

--- Shopping/Shopping/Controllers/OrdersController.cs
+++ Shopping/Shopping/Controllers/OrdersController.cs
@@ -9,7 +9,7 @@ using Vereyon.Web;
 
 namespace Shopping.Controllers
 {
-    [Authorize(Roles = "Admin")]
+  
     public class OrdersController : Controller
     {
         private readonly DataContext _context;
@@ -22,6 +22,8 @@ namespace Shopping.Controllers
             _flashMessage = flashMessage;
             _ordersHelper = ordersHelper;
         }
+
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Index()
         {
             return View(await _context.Sales
@@ -31,7 +33,7 @@ namespace Shopping.Controllers
                 .ToListAsync());
         }
 
-
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -52,7 +54,7 @@ namespace Shopping.Controllers
 
             return View(sale);
         }
-
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Dispatch(int? id)
         {
             if (id == null)
@@ -81,6 +83,8 @@ namespace Shopping.Controllers
             return RedirectToAction(nameof(Details), new { Id = sale.Id });
         }
 
+
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Send(int? id)
         {
             if (id == null)
@@ -109,6 +113,8 @@ namespace Shopping.Controllers
             return RedirectToAction(nameof(Details), new { Id = sale.Id });
         }
 
+
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Confirm(int? id)
         {
             if (id == null)
@@ -137,6 +143,7 @@ namespace Shopping.Controllers
             return RedirectToAction(nameof(Details), new { Id = sale.Id });
         }
 
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Cancel(int? id)
         {
             if (id == null)
@@ -163,6 +170,39 @@ namespace Shopping.Controllers
             return RedirectToAction(nameof(Details), new { Id = sale.Id });
         }
 
+        [Authorize(Roles = "User")]
+        public async Task<IActionResult> MyOrders()
+        {
+            return View(await _context.Sales
+                .Include(s => s.User)
+                .Include(s => s.SaleDetails)
+                .ThenInclude(sd => sd.Product)
+                .Where(s => s.User.UserName == User.Identity.Name)
+                .ToListAsync());
+        }
+
+
+        [Authorize(Roles = "User")]
+        public async Task<IActionResult> MyDetails(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Sale sale = await _context.Sales
+                .Include(s => s.User)
+                .Include(s => s.SaleDetails)
+                .ThenInclude(sd => sd.Product)
+                .ThenInclude(p => p.ProductImages)
+                .FirstOrDefaultAsync(s => s.Id == id);
+            if (sale == null)
+            {
+                return NotFound();
+            }
+
+            return View(sale);
+        }
 
     }
 
